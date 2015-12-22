9b1c5a72aa3fa746a01d91eb93de06ed538fb8e1
freddyheredia4 <none@none>
2015-12-22 13:13:40 -0500

    FreddyHeredia4: Se agrega metodo para calcular los dias entre valido desde y hasta de los atributos

--- src/org/eevolution/model/MHRProcess.java
+++ src/org/eevolution/model/MHRProcess.java
@@ -2924,5 +2924,63 @@ public class MHRProcess extends X_HR_Process implements DocAction {
 		attribute.saveEx();
 
 	}// getaddAttributeAmt
+	
+	/**
+	 * Helper Method : Get Attribute [get Attribute to search key concept and
+	 * date ]
+	 * 
+	 * @param pConcept
+	 *            - Value to Concept
+	 * @param date1
+	 * @param date2
+	 * @return Amount of concept, applying to employee
+	 */
+	public double getAttributeDaysBetween(String pConcept, Timestamp date1, Timestamp date2) {
+		MHRConcept concept = MHRConcept.forValue(getCtx(), pConcept);
+		if (concept == null)
+			return 0;
+		ArrayList<Object> params = new ArrayList<Object>();
+		StringBuilder whereClause = new StringBuilder();
+		// check ValidFrom:
+		whereClause.append(MHRAttribute.COLUMNNAME_ValidFrom + "<=?");
+		params.add(date2);
+		// check client
+		whereClause.append(" AND AD_Client_ID = ?");
+		params.add(getAD_Client_ID());
+		// check concept
+		whereClause
+				.append(" AND EXISTS (SELECT 1 FROM HR_Concept c WHERE c.HR_Concept_ID=HR_Attribute.HR_Concept_ID AND HR_Attribute.IsActive='Y' AND c.Value = ? "
+						+ " AND (HR_Attribute.validto IS NULL OR HR_Attribute.validto >= ?) )");
+		params.add(pConcept);
+		params.add(date1);
+		//
+		if (!concept.getType().equals(MHRConcept.TYPE_Information)) {
+			whereClause.append(" AND " + MHRAttribute.COLUMNNAME_C_BPartner_ID
+					+ " = ?");
+			params.add(m_C_BPartner_ID);
+		}
+		// LVE Localizacion Venezuela
+		// when is employee, it is necessary to check if the organization of the
+		// employee is equal to that of the attribute
+		// if (concept.isEmployee()){
+		whereClause.append(" AND ( " + MHRAttribute.COLUMNNAME_AD_Org_ID
+				+ "=? OR " + MHRAttribute.COLUMNNAME_AD_Org_ID + "= 0 )");
+		params.add(getAD_Org_ID());
+		// }
+
+		MHRAttribute attribute = new Query(getCtx(), MHRAttribute.Table_Name,
+				whereClause.toString(), get_TrxName()).setParameters(params)
+				// .setOrderBy(MHRAttribute.COLUMNNAME_ValidFrom + " DESC")
+				.setOrderBy(MHRAttribute.COLUMNNAME_AD_Org_ID + " DESC")
+				.first();
+		if (attribute != null){
+			if (attribute.getValidTo()!=null)
+				return getDays(attribute.getValidFrom(),attribute.getValidTo());
+		}
+			
+
+		// something else
+		return 0.0; // TODO throw exception ??
+	} // getAttribute
 
 } // MHRProcess
